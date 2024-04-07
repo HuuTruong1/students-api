27f136b484d949045dee530d6c8f49bd1c13386e
HuuTruong1 <huutruong1410@gmail.com>
2024-04-07 08:55:26 +0700

    lasted

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
 // Import MySQL module
 const express = require('express');
 const mysql = require('mysql2');
-require ('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 
 // Create connection configuration
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST, 
-  user: process.env.DB_USER, 
-  password: process.env.DB_PWD,
-  database: process.env.DB_NAME 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME
 });
 
 // Connect to MySQL database
 connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database: ', err);
+    return;
+  }
+  console.log('Connected to MySQL database!');
+});
+
+// Define route to get students data as JSON
+app.get('/', (req, res) => {
+  // Execute MySQL query to get students data
+  connection.query('SELECT * FROM students', (err, results) => {
     if (err) {
-      console.error('Error connecting to MySQL database: ', err);
+      console.error('Error executing query: ', err);
+      res.status(500).json({ error: 'Internal server error' });
       return;
     }
-    console.log('Connected to MySQL database!');
+    // Send JSON response with students data
+    res.json(results);
   });
-  
-  // Define route to get students data as JSON
-  app.get('/', (req, res) => {
-    // Execute MySQL query to get students data
-    connection.query('SELECT * FROM students', (err, results) => {
-      if (err) {
-        console.error('Error executing query: ', err);
-        res.status(500).json({ error: 'Internal server error' });
-        return;
-      }
-      // Send JSON response with students data
-      res.json(results);
-    });
-  });
-  
-  // Set up server to listen on port 3000
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+});
+
+// Set up server to listen on port 3000
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
